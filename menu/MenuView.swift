287d6b7137572553d69151a154a41f8257f35a6f
perlakay <perla.kay22@gmail.com>
2025-01-15 20:20:10 -0500

    initial setup/commit

---
+++ menu/MenuView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menu
+//
+//  Created by Perly Dahan on 1/15/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MenuView()
+}
---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by Perly Dahan on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
