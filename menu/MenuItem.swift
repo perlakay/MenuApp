7aafc19c3a9b666b9de9ff6938fa354206129a6c
perlakay <perla.kay22@gmail.com>
2025-01-21 13:53:49 -0500

    added menu funationality

--- menu/MenuItem.swift
+++ menu/MenuItem.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
-struct MenuItem { //becomes a custom data type
+struct MenuItem: Identifiable { //informing that it conforms to a protocol  //becomes a custom data type
+    
+    var id: UUID = UUID() //creates a random id which is assigned to each instance of the MenuItem type 
     var name:String
     var price:String
-    var ImageName:String
+    var imageName:String
+    
 }
