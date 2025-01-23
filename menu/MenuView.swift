187fbe8fe97d516619669d0cbc5893cdb22a8db6
perlakay <perla.kay22@gmail.com>
2025-01-23 15:56:41 -0500

    finished project

--- menu/MenuView.swift
+++ menu/MenuView.swift
@@ -15,28 +15,10 @@ struct MenuView: View {
         
         List(menuItems) { item in
             
-            HStack{
-                Image(item.imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height:50)
-                    .cornerRadius(10)
-                
-                Text(item.name)
-                    .bold()
-                
-                Spacer()
-                
-                Text("$" + item.price)
-                
-                
-            }
-            .listRowSeparator(.hidden)
-            .listRowBackground(
-                Color(.brown)
-                    .opacity(0.1)
-                
-            )
+            MenuListRow(item: item)
+            
+            
+            
         }
         .listStyle(.plain)
         .onAppear {
